e1cf4ceedbbf8290e4a2ef71f2d8c15be7cdc70a
Jaap Joris Vens <jj@rtts.eu>
2018-12-25 12:40:29 +0100

    new --slideshow command line argument

--- ialauncher/backend.py
+++ ialauncher/backend.py
@@ -14,9 +14,10 @@ class Backend(TCPServer):
     allow_reuse_address = True
     games = {}
 
-    def __init__(self, games_dir, letter=None, aspect=False):
+    def __init__(self, games_dir, letter=None, aspect=False, slideshow=None):
         self.letter = letter
         self.aspect = aspect
+        self.slideshow = slideshow
         self.load_games(games_dir)
         super().__init__(('localhost', 11236), GameHandler)
 
@@ -67,10 +68,14 @@ class GameHandler(StreamRequestHandler):
             games_list = self.server.get_games()
             initial_grid = 20
             aspect = self.server.aspect
+            if self.server.slideshow:
+                slideshow = self.server.slideshow * 1000
+                initial_grid = 100
             self.wfile.write(T.render({
                 'games': games_list,
                 'initial_grid': initial_grid,
                 'aspect': aspect,
+                'slideshow': slideshow,
             }).encode('utf8'))
 
         else:
