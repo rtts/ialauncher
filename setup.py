344b2005f33f57e1057a5b9ab610351bba6ad7d8
Jaap Joris Vens <jj@rtts.eu>
2020-12-26 10:36:21 +0100

    Require Python 3.8 (because of the walrus operator!)

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
     ],
+    python_requires = '>=3.8',
     install_requires = [
         'pygame',
     ],
