74a568c18d1476defb5075c4e0503e58ca157283
Jaap Joris Vens <jj@rtts.eu>
2020-12-31 17:05:17 +0100

    Search for DOSBox The Right Way™ on macOS
    Ref. #10

--- ialauncher/dosbox.py
+++ ialauncher/dosbox.py
@@ -1,47 +1,42 @@
-import os, sys, glob, subprocess
+import os, glob, subprocess
 
-error = """
 
-Uh-oh! The program DOSBox could not be found on your system. IA Launcher acts as a frontend for DOSBox: when you select a game, it starts DOSBox with the right arguments to play that game.
+class DOSBoxNotFound(Exception):
+    pass
 
-Please visit https://www.dosbox.com/ to learn more about DOSBox and download the correct installer for your operating system.
-"""
 
-def try_path(path):
-    subprocess.run([path, '--version'], capture_output=True).check_returncode()
-    return path
+def try_command(command):
+    subprocess.run(command + ['--version'], capture_output=True).check_returncode()
+    return command
+
 
 def get_dosbox_path():
     try:
-        return try_path('dosbox')
+        return try_command(['dosbox'])
     except:
         pass
     try:
-        # Special case for macOS
-        return try_path('/Applications/dosbox.app/Contents/MacOS/DOSBox')
+        # Proper way to do it on macOS
+        return try_command(['open', '-a', 'DOSBox', '--args'])
     except:
         pass
     try:
-        # Another special case for macOS
-        return try_path('open -a DOSBox')
+        # Fallback to hardcoded path on macOS
+        return try_command(['/Applications/dosbox.app/Contents/MacOS/DOSBox'])
     except:
         pass
     try:
         # Special case for Windows
         pf = os.environ['ProgramFiles(x86)']
         path = glob.glob(f'{pf}\dosbox*\dosbox.exe')[0]
-        return try_path(path)
+        return try_command([path])
     except:
         pass
 
-    try:
-        print(error)
-        input("Press any key to exit...") # fails on Windows
-    except:
-        class DOSBoxNotFound(Exception):
-            pass
+    raise DOSBoxNotFound("""
 
-        # This will show the error in a popup window. Ugly, but sufficient.
-        raise DOSBoxNotFound(error)
+Uh-oh! The program DOSBox could not be found on your system. IA Launcher acts as a frontend for DOSBox: when you select a game, it starts DOSBox with the right arguments to play that game.
 
-    sys.exit(1)
+Please visit https://www.dosbox.com/ to learn more about DOSBox and download the correct installer for your operating system.
+
+""")
